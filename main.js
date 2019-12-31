0e56f2ae883a01d458d0602e553b3e394d0d16e3
tbg <54632118+iamtb@users.noreply.github.com>
2019-12-30 21:57:18 -0500

    update

--- main.js
+++ main.js
@@ -10,48 +10,53 @@
 
 
 var map;
+var marker;
 var lat, lng;
 var latLon;
 var IP;
+var coords;
 
+// get ip to be used from form
+function validate(){
+    var input = document.forms["ipForm"]["ip"].value;
+    console.log(input);
+    if (form == "" || input == null){
+        alert("Please enter an ip address.")
+        return false;
+    }
+    else{
+        doLookup(input);
+    }
+}
+
+// save json response
 function doLookup(IP){
+    console.log(IP);
     if (IP==null){
         IP = '8.8.8.8'
     }
     else{
-        IP = document.getElementById('ip');
+        IP = document.getElementById('ip').ip;
     }
-    const url='https://www.ipinfo.io/' + IP + '?token=7fcba63eeb5f7a';
+    console.log(IP);
+
+    const url='https://www.ipinfo.io/' + IP + '?token=secret';
+    
     fetch(url)
     .then(data=>{return data.json()})
     .then(res=>{ console.log(res)})
-    .catch (error=>console.log(error);
+    .catch (error=>{console.log(error)})
+
 }
-
-    // get ip to be used from form
-    // get ipinfo using ip http request
-    // get lat & lng from http response
-
-    // initMap(lat, lng);
-
-
-function initMap(lat, lng){
-    navigator.geolocation.getCurrentPosition(function(position){
-    lat = Number(position.coords.latitude.toFixed(2));
-    lng = Number(position.coords.longitude.toFixed(2));
-
-    if (lat && lng){
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: lat, lng: lng},
-            zoom: 10,
-            mapTypeId: 'satellite'
-        });
-    }
-    else{
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 42.26, lng: -72.06},
-            zoom: 10,
-            mapTypeId: 'satellite'
-        });
-    }
-})};
+// parse response
+// // if enabled 
+// //     navigator.geolocation.getCurrentPosition(function(position){
+//     lat = Number(position.coords.latitude.toFixed(2));
+//     lng = Number(position.coords.longitude.toFixed(2));
+    
+// recreate map
+function initMap(){
+    coords = {lat: 42.2626, lng: -71.8023};
+    map = new google.maps.Map(document.getElementById('map'), { zoom: 10, center: coords});
+    marker = new google.maps.Marker({position: coords, map: map});
+};
